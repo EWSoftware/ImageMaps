168750b9866785453a7f38df091ff970a3c484f8
Eric Woodruff <Eric@EWoodruff.us>
2017-12-29 13:44:05 -0800

    Documentation project update

--- MasterBuild.bat
+++ MasterBuild.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
 CLS
 
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m /m "Source\EWSImageMaps.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+REM Use the earliest version of MSBuild available
+IF EXIST "%ProgramFiles(x86)%\MSBuild\14.0" SET "MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
+IF EXIST "%ProgramFiles(x86)%\MSBuild\12.0" SET "MSBUILD=%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
 
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m /m "Demos\WinFormsDemos.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+"%MSBUILD%" /nologo /v:m /m "Source\EWSImageMaps.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
 
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m /m "Demos\WebDemos.sln" /t:Clean;Build
+"%MSBUILD%" /nologo /v:m /m "Demos\WinFormsDemos.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
 
-IF NOT "%SHFBROOT%"=="" "%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m "Doc\EWSoftwareImageMaps.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+"%MSBUILD%" /nologo /v:m /m "Demos\WebDemos.sln" /t:Clean;Build
+
+IF NOT "%SHFBROOT%"=="" "%MSBUILD%" /nologo /v:m "Doc\EWSoftwareImageMaps.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
 
 IF "%SHFBROOT%"=="" ECHO **** Sandcastle help file builder not installed.  Skipping help build. ****
 
